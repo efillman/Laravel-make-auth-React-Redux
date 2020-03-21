da5fd5a2018a97c75d511b5b470d603e6228b340
Evan Fillman <efillman@gmail.com>
2020-03-21 17:52:33 -0500

    all auth apis working

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 //use App\Notifications\SignupActivate;
 
 class AuthController extends Controller
@@ -147,7 +152,153 @@ class AuthController extends Controller
         $request->user()->sendEmailVerificationNotification();
 
         $response = ['message' => 'We have e-mailed your new activation code'];
-        return response($response);
+        return response($response, 200);
+    }
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function sendResetLinkEmail(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+          'email' => 'required|email'
+        ]);
+
+        if ($validator->fails()) {
+            return response(['error'=>$validator->errors()->all()], 422);
+        }
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = $this->broker()->sendResetLink(
+            $this->credentials($request)
+        );
+
+        $response == Password::RESET_LINK_SENT;
+
+        if (!$response) {
+          $response = ['error' => 'Invalid Email'];
+          return response($response, 404);
+        }
+
+       if ($response) {
+         $response = ['message' => 'We have e-mailed your reset code'];
+         return response($response, 200);
+       }
+
+    }
+
+    /**
+     * Get the needed authentication credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        return $request->only('email');
+    }
+
+    /**
+     * Get the broker to be used during password reset.
+     *
+     * @return \Illuminate\Contracts\Auth\PasswordBroker
+     */
+    public function broker()
+    {
+        return Password::broker();
+    }
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function reset(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+          'token' => 'required',
+          'email' => 'required|email',
+          'password' => 'required|confirmed|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response(['error'=>$validator->errors()->all()], 422);
+        }
+
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+        $response = $this->broker()->reset(
+            $this->resetCredentials($request), function ($user, $password) {
+                $this->resetPassword($user, $password);
+            }
+        );
+
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        $response == Password::PASSWORD_RESET;
+
+        if (!$response) {
+          $response = ['error' => 'Invalid Email or Token'];
+          return response($response, 404);
+        }
+
+       if ($response) {
+         $user = User::where('email', $request['email'])->first();
+         $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+         $response = ['token' => $token];
+         return response($response, 200);
+       }
+    }
+
+    /**
+     * Get the password reset credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function resetCredentials(Request $request)
+    {
+        return $request->only(
+            'email', 'password', 'password_confirmation', 'token'
+        );
+    }
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $password
+     * @return void
+     */
+    protected function resetPassword($user, $password)
+    {
+        $this->setUserPassword($user, $password);
+
+        $user->setRememberToken(Str::random(60));
+
+        $user->save();
+
+        event(new PasswordReset($user));
+    }
+
+    /**
+     * Set the user's password.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $password
+     * @return void
+     */
+    protected function setUserPassword($user, $password)
+    {
+        $user->password = Hash::make($password);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,5 @@ Route::get('/', function () {
 Route::get('/home', 'HomeController@index')->name('home');
 
 //temporary for testing
-Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
-Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+Route::get('password/reset/{token}', 'Auth\VerificationController@reset')->name('password.reset');
